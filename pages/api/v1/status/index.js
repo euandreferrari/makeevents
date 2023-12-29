8a686bd44217ebebf9c421a7a2a9b6f4d294f0f8
euandreferrari <152464598+euandreferrari@users.noreply.github.com>
2023-12-29 02:15:40 +0000

    Requisicoes ao DB

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "alunos do curso" });
+  const updateAt = new Date().toISOString();
+
+  // versao do postgres
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  // conexoes usadas do postgres
+
+  // conexoes maximas do postgres
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  response.status(200).json({
+    updated_at: updateAt,
+    dependencies: {
+      version: databaseVersionValue,
+      max_connections: databaseMaxConnectionsValue,
+    },
+  });
 }
 
 export default status;
